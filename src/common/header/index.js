12bf5034b6689311547f697bd909dd1059580df4
zhang wei <2923089913@qq.com>
2023-03-01 17:43:45 +0800

    '使用redux进行数据管理'

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,57 +1,57 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { SearchWrapper, Addition, Button, HeaderWrapper, Logo, Nav, NavItem, NavSearch } from './style'
 import { CSSTransition } from 'react-transition-group'
+import { connect } from 'react-redux'
+import { actionCreators } from './store'
 
-class Header extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      focused: false
-    }
-  }
-  render() {
-    return (
-      <HeaderWrapper>
-        <Logo />
-        <Nav>
-          <NavItem className='left active'>首页</NavItem>
-          <NavItem className='left'>下载</NavItem>
-          <NavItem className='right'>
-            <i className='iconfont icon-Aa'></i>
-          </NavItem>
-          <NavItem className='right'>登录</NavItem>
-          <SearchWrapper>
-            <CSSTransition
-              in={this.state.focused}
-              timeout={200}
-              classNames='slide'
-            >
-              <NavSearch onFocus={() => this.handleInputFocus()} onBlur={() => this.handleInputBlur()} className={this.state.focused ? 'focused' : ''}></NavSearch>
-            </CSSTransition>
-            <i className={this.state.focused ? 'focused iconfont icon-fangdajing' : 'iconfont icon-fangdajing'} ></i>
-          </SearchWrapper>
-        </Nav>
-        <Addition>
-          <Button className='writting'>
-            <i className='iconfont icon-quill-pen-fill'></i>
-            写文章
-          </Button>
-          <Button className='reg'>注册</Button>
-        </Addition>
-      </HeaderWrapper>
-    )
-  }
-  handleInputFocus() {
-    console.log('pppp');
-    this.setState({
-      focused: true
-    })
-  }
-  handleInputBlur() {
-    this.setState({
-      focused: false
-    })
+const Header = (props) => {
+  return (
+    <HeaderWrapper>
+      <Logo />
+      <Nav>
+        <NavItem className='left active'>首页</NavItem>
+        <NavItem className='left'>下载</NavItem>
+        <NavItem className='right'>
+          <i className='iconfont icon-Aa'></i>
+        </NavItem>
+        <NavItem className='right'>登录</NavItem>
+        <SearchWrapper>
+          <CSSTransition
+            in={props.focused}
+            timeout={200}
+            classNames='slide'
+          >
+            <NavSearch onFocus={props.handleInputFocus} onBlur={props.handleInputBlur} className={props.focused ? 'focused' : ''}></NavSearch>
+          </CSSTransition>
+          <i className={props.focused ? 'focused iconfont icon-fangdajing' : 'iconfont icon-fangdajing'} ></i>
+        </SearchWrapper>
+      </Nav>
+      <Addition>
+        <Button className='writting'>
+          <i className='iconfont icon-quill-pen-fill'></i>
+          写文章
+        </Button>
+        <Button className='reg'>注册</Button>
+      </Addition>
+    </HeaderWrapper>
+  )
+}
+
+const mapStateToProps = (state) => {
+  return {
+    focused: state.get('header').get('focused')
   }
 }
 
-export default Header
+const mapDispathToProps = (dispath) => {
+  return {
+    handleInputFocus() {
+      dispath(actionCreators.searchFocus())
+    },
+    handleInputBlur() {
+      dispath(actionCreators.searchBlur())
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispathToProps)(Header)
---
+++ src/common/header/store/reducer.js
@@ -0,0 +1,18 @@
+import { fromJS } from 'immutable'
+import * as actionTypes from './actionTypes'
+
+const defaultState = fromJS({
+  focused: false
+})
+
+export default (state = defaultState, action) => {
+  if (action.type === actionTypes.SEARCH_FOCUS) {
+    // immutable对象的set方法，会结合之前immutable对象的值
+    //和设置的值,返回一个全新的对象
+    return state.set('focused', true)
+  }
+  if (action.type === actionTypes.SEARCH_BLUR) {
+    return state.set('focused', false)
+  }
+  return state
+}
